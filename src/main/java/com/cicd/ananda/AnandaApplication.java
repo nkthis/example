f8f0f2fb3d6a1e2daaf7e3cd0eb77a768de5f74a
VortexProjects <vortex1649@gmail.com>
2024-02-12 15:56:24 +0530

    first commit

---
+++ src/main/java/com/cicd/ananda/AnandaApplication.java
@@ -0,0 +1,13 @@
+package com.cicd.ananda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnandaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cicd/ananda/AnandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.ananda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
